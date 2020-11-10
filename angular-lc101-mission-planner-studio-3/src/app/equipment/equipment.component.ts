b28b62777a850e379d5baeae1ec5fbc4ca563f9e
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-11-09 19:59:26 -0600

    added code for the studio

--- angular-lc101-mission-planner-studio-3/src/app/equipment/equipment.component.ts
+++ angular-lc101-mission-planner-studio-3/src/app/equipment/equipment.component.ts
@@ -21,11 +21,24 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   closeToFull: boolean = false;
 
    constructor() { }
 
    ngOnInit() { }
 
    // Code your addItem function here:
+
+   addItem(item: object): boolean {
+      this.cargoHold.push(item);
+      this.cargoMass += item['mass'];
+      if(this.maximumAllowedMass - this.cargoMass <= 200) {
+        this.closeToFull = true;
+        return true;
+      } else {
+        this.closeToFull = false;
+        return false;
+      }
+   };
    
 }
